2f56d27d0d3aaf56c946e37933030e944597062f
padhiarmeet <padhiarmeet00@gmail.com>
2025-06-12 09:29:43 +0530

    12/06/2025

--- dimond.java
+++ dimond.java
@@ -1,3 +1,2 @@
-public class dimond {
-    
-}
+public class dimond {  
+} 
---
+++ self_learning/First.java
@@ -0,0 +1,15 @@
+package self_learning;
+
+class First {
+    public static void main(String[] args) {
+        System.out.println(2 +7);
+        char name = 'm';
+        System.out.println(name+'m');
+    }
+} 
+
+class Second{
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
