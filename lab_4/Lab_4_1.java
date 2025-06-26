c542671dd4787e0455515f02e312ff0a9a0622cc
padhiarmeet <padhiarmeet00@gmail.com>
2025-06-26 09:09:44 +0530

    26/06/2025

--- lab_4/Lab_4_1.java
+++ lab_4/Lab_4_1.java
@@ -5,21 +5,16 @@ public class Lab_4_1 {
         int[][] adj = { { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 } };
         int count = 0;
         int n = adj.length;
-        boolean count0 = false;
+        
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (adj[i][j] == 1)
                     count++;
-                else
-                    count0 = true;
             }
         }
-        if (!count0) {
-            System.out.println("Total group is - 1");
-        } 
-        else {
+        
             if (count == 0) {
-                for (int i = 0; i < n; i++) {
+                for (int i = 1; i < n; i++) {
                     if (adj[i][i] == 1)
                         count++;
                 }
@@ -29,6 +24,5 @@ public class Lab_4_1 {
                 System.out.println("Total group is - " + count);
 
             }
-        }
     }
 }
